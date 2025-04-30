cab4c2cea131767bbe935af78eb88a93e1dee7ae
Kewinv <kevin2005lez@gmail.com>
2025-04-30 09:00:00 -0500

    Aplicando metodos a CuentaService y trabajo en application.propieties

---
+++ src/main/java/com/fortbank/ATM/entity/Cliente.java
@@ -0,0 +1,39 @@
+package com.fortbank.ATM.entity;
+
+import java.util.List;
+
+import jakarta.annotation.Generated;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+public class Cliente 
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nombre;
+    @Column(unique = true)
+    private String identificacion;
+    private String pin;
+    private boolean bloqueado;
+    private int intentosFallidos;
+    @OneToMany(mappedBy = "cliente", cascade = CascadeType.ALL)
+    private List<Cuenta> cuentas;
+}
