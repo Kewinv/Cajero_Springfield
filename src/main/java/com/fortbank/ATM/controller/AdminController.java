1c70c2c96650e639bff3e7cfd9e5a17db60ed221
Kewinv <kevin2005lez@gmail.com>
2025-05-14 08:56:02 -0500

    Nuevos html, crear cuenta y desbloquear

--- src/main/java/com/fortbank/ATM/controller/AdminController.java
+++ src/main/java/com/fortbank/ATM/controller/AdminController.java
@@ -1,11 +1,16 @@
 package com.fortbank.ATM.controller;
 
+import com.fortbank.ATM.entity.Cliente;
 import com.fortbank.ATM.services.ClienteService;
 import com.fortbank.ATM.services.CuentaService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 
 @Controller
 @RequiredArgsConstructor
@@ -18,6 +23,19 @@ public class AdminController {
     @GetMapping // Creación de rutas, define lo que ira despues de "/admin"
     public String adminHome(){
         return "admin/index"; // Retorna la vista "admin/index.html"
+    }
+
+    @GetMapping("/crear-cliente")
+    public String mostrarFormularioCliente(Model model){
+        model.addAttribute("cliente", new Cliente());
+        return "admin/crear-cliente";
+
+    }
+
+    @PostMapping("/crear-cliente")
+    public String crearCliente(@ModelAttribute("cliente") Cliente cliente){
+        clienteService.crearCliente(cliente);
+        return "redirect:/admin"; // Redirige a la ruta "/admin"
     }
 
 }
